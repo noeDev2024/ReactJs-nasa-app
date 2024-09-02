7dfdb46dc181221e959399dcce8a12bce37b8788
noeDev2024 <noeandimat@gmail.com>
2024-09-03 07:22:31 +0800

    adding className to Sidebar comp

--- reactjs-nasa-application/src/components/Sidebar.jsx
+++ reactjs-nasa-application/src/components/Sidebar.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 export default function Sidebar() {
   return (
-    <div>
-        <h2>The Brutal Martian Landscape</h2>
-        <div>
-            <p>Description</p>
+    <div className='sidebar'>
+        <div className='bgOverlay'></div>
+        <div className='sidebarContents'>
+            <h2>The Brutal Martian Landscape</h2>
+            <div>
+                <p>Description</p>
+            </div>
+            <button>
+                <i className="fa-solid fa-arrow-right"></i>
+            </button>
         </div>
     </div>
   )

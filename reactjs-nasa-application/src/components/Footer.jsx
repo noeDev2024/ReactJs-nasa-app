680bfef4e3c65f44c0cd342e6c24eb35981b2d43
noeDev2024 <noeandimat@gmail.com>
2024-09-03 06:34:35 +0800

    Creating Components

---
+++ reactjs-nasa-application/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <div>
+            <h2>The Brutal martian Landscape</h2>
+            <h1>APOD PROJECT</h1>
+        </div>
+    </footer>
+  )
+}

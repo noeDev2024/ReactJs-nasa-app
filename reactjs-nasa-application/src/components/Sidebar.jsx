869e44d551366bb56a151001bcc97ce1030f69fc
noeDev2024 <noeandimat@gmail.com>
2024-09-03 07:01:05 +0800

    adding content to sidebar comp

--- reactjs-nasa-application/src/components/Sidebar.jsx
+++ reactjs-nasa-application/src/components/Sidebar.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Sidebar() {
   return (
     <div>
-        
+        <h2>The Brutal Martian Landscape</h2>
+        <div>
+            <p>Description</p>
+        </div>
     </div>
   )
 }
